c873c4994c95c26d68d9bbe3a5d94487bc2720b5
0921m <0921m@DESKTOP-TGOS6TP>
2022-12-05 17:33:04 +0900

    1.0 upload

---
+++ src/start/Start.java
@@ -0,0 +1,10 @@
+package start;
+
+import controller.MainController;
+
+public class Start {
+	public static void main(String[] args) {
+		MainController mc = new MainController();
+		mc.main();
+	}
+}
